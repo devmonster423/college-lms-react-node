b51121048330b1828a0b3f30e03f2daee7aaa876
devmonster423 <devmonster423@gmail.com>
2018-01-29 11:28:12 +0530

    Added server for public files.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 //  Generated Imports
 const { adminRoutes } = require('./routes/adminRoutes');
@@ -15,11 +16,13 @@ const app = express();
 
 const port = 3000 || process.env.PORT;
 
+const publicPath = path.join(__dirname, '..', 'public');
+
 //  Adding middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(passport.initialize());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(publicPath));
 
 //  Routing
 app.get('/', (req, res) => res.render('index'));
@@ -27,5 +30,8 @@ app.use('/admin/', adminRoutes);
 app.use('/teacher/', teacherRoutes);
 app.use('/student/', studentRoutes);
 app.use('/visitor/', visitorRoutes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.listen(port, () => console.log(`Server is up on the ${port}`)); // eslint-disable-line
