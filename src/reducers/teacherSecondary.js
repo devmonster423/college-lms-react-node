402eecd327f0d2cfda41917623e2b20924d3117c
devmonster423 <devmonster423@gmail.com>
2018-02-15 16:25:22 +0530

    Deleted Comments.

--- src/reducers/teacherSecondary.js
+++ src/reducers/teacherSecondary.js
@@ -39,28 +39,3 @@ export default (state = teacherSecondaryDefaultState, action) => {
       return state;
   }
 };
-
-// export default (state = secondaryDefaultStore, action) => {
-//   switch (action.type) {
-//     case 'SET_TEACHER_SECONDARY':
-//       return { ...action.teacherSecondary };
-//     case 'ADD_WORk':
-//       return { ...state, work: action.work };
-//     case 'EDIT_WORK':
-//       return { ...state, work: action.work };
-//     case 'ADD_TECHNICALSKILL':
-//       return { ...state, skill: action.skills };
-//     case 'EDIT_TECHNICALSKILL':
-//       return { ...state, skill: action.skills };
-//     case 'ADD_COMMITTE':
-//       return { ...state, committe: action.committe };
-//     case 'EDIT_COMMITTE':
-//       return { ...state, committe: action.committe };
-//     case 'UPDATE_SPECIALISATION':
-//       return { ...state, specialisation: action.specialisation };
-//     case 'UPDATE_EDUCATION':
-//       return { ...state, education: action.education };
-//     default:
-//       return state;
-//   }
-// };
