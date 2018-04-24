a1f8eaf8f24ba6edf566cbe4f1beca2991cbe4cf
devmonster423 <devmonster423@gmail.com>
2018-04-24 01:12:49 -0700

    faculty page

---
+++ src/pages/staticPages/Facility.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Page } from 'theme/Components';
+import { UpperData ,LowerData } from './../../components/static/facility/FacilityData';
+
+export default () => (
+  <Page>
+      <UpperData />
+      <br />
+      <br />
+    <LowerData />
+  </Page>
+);
+ 
