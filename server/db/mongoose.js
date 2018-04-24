e8bbe07e5946c99a44832f5360aaf54141cf16b1
devmonster423 <devmonster423@gmail.com>
2018-04-24 13:27:31 +0530

    Added test configs.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-require('dotenv').config({ path: '.env' });
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else {
+  require('dotenv').config({ path: '.env' });
+}
 
 mongoose.Promise = global.Promise;
 mongoose
