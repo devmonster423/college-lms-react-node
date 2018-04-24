464473927667393da6bc4508858b1349dc992960
devmonster423 <devmonster423@gmail.com>
2018-04-24 01:11:34 -0700

    rti page

---
+++ src/pages/staticPages/RtiPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import  Rti from './../../components/static/Rti/RtiData';
+
+import { Page } from 'theme/Components';
+
+
+export default ()=>(
+    <Page>
+        <Rti />
+        </Page>
+);
