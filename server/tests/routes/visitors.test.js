cc0bb48ce143d8d3fca4714306df9d1ed83217d6
devmonster423 <devmonster423@gmail.com>
2018-04-28 23:04:03 +0530

    urls fiels and 2 visitors routes tested

--- server/tests/routes/visitors.test.js
+++ server/tests/routes/visitors.test.js
@@ -2,35 +2,72 @@ const request = require('supertest');
 const { app } = require('./../../server');
 
 //  Importing Models
+
 const { Notifications } = require('./../../models/notification');
 const { saveMinimal2 } = require('./../../utils/utils');
 
+// ------ importing urls ----
+
+const {
+  getnotifications,
+  // getallnotifications,
+  // getallQuestionPaper,
+  // getevents,
+  // getallevents,
+  // getsyll,
+  // gettt,
+  // getteacher,
+  // getstudent,
+  // searchstudentbyname,
+  // searchstudentbyrollno,
+  // verifyemail,
+} = require('./../../utils/urls');
+
 const notification = {
-  title: 'Voluptatum modi facere voluptatibus non ducimus veritatis.',
-  description:
-    'Neque deserunt ut libero eaque quia. Eos rerum magni. In consequuntur doloremque sunt similique.',
-  file: 'http://lorempixel.com/640/480',
-  tags: ['tag1', 'tag2', 'tag3'],
-  link: 'https://herta.name',
+  title: 'Voluptatum ',
+  description: 'Neque ',
+  file: 'http:',
+  tags: ['tag1'],
+  link: 'https',
 };
 
 const saveNotificationsMinimal = saveMinimal2(Notifications);
-
-beforeEach(() => saveNotificationsMinimal(notification));
-
-afterEach(() => Notifications.remove({}));
 
 afterAll(() => {
   app.server.close();
 });
 
-describe('Visitors Routes', () => {
-  test('should be a sample test', (done) => {
+beforeEach(() => saveNotificationsMinimal(notification));
+
+afterEach(() => Notifications.remove({}));
+
+describe('-- testing the visitors routes ', () => {
+  beforeEach(() => saveNotificationsMinimal(notification));
+  beforeEach(() => saveNotificationsMinimal(notification));
+  beforeEach(() => saveNotificationsMinimal(notification));
+  beforeEach(() => saveNotificationsMinimal(notification));
+  afterEach(() => Notifications.remove({}));
+  test('get all notification', (done) => {
+    console.log('1  test run');
     request(app)
-      .get('/s/visitor/getallnotifications')
+      .get(getnotifications)
       .expect((res) => {
-        expect(res.body).toHaveLength(1);
-        expect(res.body).toHaveLength(1);
+        expect(res.body).toHaveLength(3);
+      })
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        return done();
+      });
+  });
+
+  test('get all 3 latest notificaionts', (done) => {
+    console.log('2  test run');
+    request(app)
+      .get('/s/visitor/getnotifications')
+      .expect((res) => {
+        expect(res.body).toHaveLength(3);
       })
       .end((err) => {
         if (err) {
---
+++ server/utils/urls.js
@@ -0,0 +1,29 @@
+// ------- visitiors routes urls ---------
+
+const getnotifications = '/s/visitor/getnotifications';
+const getallnotifications = '/s/visitor/getallnotifications';
+const getallQuestionPaper = '/s/visitor/getallQuestionPaper';
+const getevents = '/s/visitor/getevents';
+const getallevents = '/s/visitor/getallevents';
+const getsyll = '/s/visitor/getsyll';
+const gettt = '/s/visitor/gettt';
+const getteacher = '/s/visitor/getteacher';
+const getstudent = '/s/visitor/getstudent';
+const searchstudentbyname = '/s/visitor/searchstudentbyname';
+const searchstudentbyrollno = '/s/visitor/searchstudentbyrollno';
+const verifyemail = '/s/visitor/verifyemail';
+
+module.exports = {
+  getnotifications,
+  getallnotifications,
+  getallQuestionPaper,
+  getevents,
+  getallevents,
+  getsyll,
+  gettt,
+  getteacher,
+  getstudent,
+  searchstudentbyname,
+  searchstudentbyrollno,
+  verifyemail,
+};
