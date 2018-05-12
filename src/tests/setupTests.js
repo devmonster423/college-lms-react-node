5b17aa18ca442393f359c0040096192b2d185267
devmonster423 <devmonster423@gmail.com>
2018-05-12 23:34:48 +0530

    git conflict fixed

--- src/tests/setupTests.js
+++
@@ -1,7 +0,0 @@
-// require('dotenv').config({ path: '.env.test' });
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({
-  adapter: new Adapter(),
-});
