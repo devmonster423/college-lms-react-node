97e2ed69c510dc7dc25f5e03fcb855db1c75e21d
devmonster423 <devmonster423@gmail.com>
2018-05-23 16:37:08 +0530

    tests completed till

--- server/routes/visitorRoutes.js
+++ server/routes/visitorRoutes.js
@@ -24,6 +24,7 @@ const visitorRoutes = express.Router();
 visitorRoutes.get('/getnotifications', getLatestNotifications);
 
 visitorRoutes.get('/getallnotifications', getAllNotifications);
+
 visitorRoutes.get('/getallQuestionPaper', getAllQuesetionPaper);
 
 visitorRoutes.get('/getevents', getLatestEvents);
--- server/tests/routes/visitors.test.js
+++ server/tests/routes/visitors.test.js
@@ -8,8 +8,11 @@ const { questionBank } = require('./../../models/questionBank');
 const { Event } = require('./../../models/events');
 const { Syllabus } = require('./../../models/syllabus');
 const { TimeTable } = require('./../../models/timeTable');
+const { StudentPrimary } = require('./../../models/studentPrimary');
+// const { Email } = require('./../../models/email');
 // -- importing the utils functions ---
-const { saveMinimal2 } = require('./../../utils/utils');
+const { saveMinimal2, saveMinimal } = require('./../../utils/utils');
+
 // const { giveLatestThreeItem } = require('./../../utils/utils');
 // ------ importing urls ----
 
@@ -22,8 +25,8 @@ const {
   getsyll,
   gettt,
   // getteacher,
-  // getstudent,
-  // searchstudentbyname,
+  getstudent,
+  searchstudentbyname,
   // searchstudentbyrollno,
   // verifyemail,
 } = require('./../../utils/urls');
@@ -74,12 +77,30 @@ const timetable = {
   file: 'dgfdgg',
 };
 
+const student = {
+  name: 'Mabel',
+  rollNo: 4202070311602303,
+  location: 'Port Constantinview',
+  dateOfBirth: 'Mon Jun 26 1995 02:27:14 GMT+0530 (India Standard Time)',
+  gender: 'male',
+  branch: 'IT',
+  email: 'hello@gmail.com',
+  admittedIn: 'Mon Sep 11 2016 01:42:00 GMT+0530 (India Standard Time)',
+  bio:
+    'Ducimus eius est ea. Dolorem adipisci aliquid nulla autem nihil voluptas. Quis saepe numquam possimus qui et ut laboriosam est. Iste eaque omnis velit nesciunt deserunt qui.',
+  auth: {
+    provider: 'Eaque assumenda molestiae quod aut saepe et.',
+    providerId: 'voluptas',
+  },
+};
+
 // -- saving th models to
 const saveQuestionPaperMinimal = saveMinimal2(questionBank);
 const saveNotificationsMinimal = saveMinimal2(Notifications);
 const saveEvent = saveMinimal2(Event);
 const saveSyllabus = saveMinimal2(Syllabus);
 const saveTimetable = saveMinimal2(TimeTable);
+const saveStudent = saveMinimal(StudentPrimary);
 afterAll(() => {
   app.server.close();
 });
@@ -112,7 +133,11 @@ describe('-- testing the visitors routes ', () => {
   beforeEach(() => saveTimetable(timetable));
   afterEach(() => TimeTable.remove({}));
 
-  //------------------------------------------------------
+  // --- functions for student ---
+  beforeEach(() => saveStudent(student));
+  afterEach(() => StudentPrimary.remove({}));
+
+  //-----------------------------------------------
 
   test('get all notification', (done) => {
     request(app)
@@ -210,4 +235,67 @@ describe('-- testing the visitors routes ', () => {
         return done();
       });
   });
+
+  test('get student ', (done) => {
+    // const name = slug(student.name);
+    request(app)
+      .post(getstudent)
+      .send({ slugg: 'mabel' })
+      .expect((res) => {
+        expect(res.body.name).toBe(student.name);
+      })
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        return done();
+      });
+  });
+
+  test('get student by name', (done) => {
+    request(app)
+      .post(searchstudentbyname)
+      .send({ name: 'mabel' })
+      .expect((res) => {
+        expect(res.body[0].name).toBe(student.name);
+      })
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        return done();
+      });
+  });
+
+  // test('get student by rollno', (done) => {
+  //   request(app)
+  //     .post(searchstudentbyrollno)
+  //     .send({ rollNo: 4202070311602303 })
+  //     .expect((res) => {
+  //       expect(res.body[0].rollNo).toBe(student.rollNo);
+  //     })
+  //     .end((err) => {
+  //       if (err) {
+  //         return done(err);
+  //       }
+  //       return done();
+  //     });
+  // });
+
+  // test('verify email', (done) => {
+  //   request(app)
+  //     .post(verifyemail)
+  //     .send({ email: 'hello@gmail.com' })
+  //     .expect(200)
+  //     .expect((res) => {
+  //       console.log(res);
+  //       expect(res.body[0]).toBeUndefined();
+  //     })
+  //     .end((err) => {
+  //       if (err) {
+  //         return done(err);
+  //       }
+  //       return done();
+  //     });
+  // });
 });
