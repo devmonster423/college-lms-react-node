a3b4442ca4c011c9bfede9de2e0c23ef0337ca93
devmonster423 <devmonster423@gmail.com>
2018-06-19 22:25:10 +0530

    Added more test.

--- server/tests/routes/Visitors/Notifications.test.js
+++ server/tests/routes/Visitors/Notifications.test.js
@@ -8,15 +8,17 @@ const { GET_ALL_NOTIFICATIONS } = require('./../../../utils/urls');
 const populateNotifications = populateDB(Notifications);
 
 describe('Visitor Routes - Notifications', () => {
-  beforeEach(() => populateNotifications([notification]));
   afterEach(() => Notifications.remove({}));
   describe(GET_ALL_NOTIFICATIONS, () => {
-    test('Get all notification.', () => {
-      request(app)
-        .get(GET_ALL_NOTIFICATIONS)
-        .expect((res) => {
-          expect(res.body).toHaveLength(1);
-        });
+    describe('Test on single notification', () => {
+      beforeEach(() => populateNotifications([notification[0]]));
+      test('Get all notification.', () =>
+        request(app)
+          .get(GET_ALL_NOTIFICATIONS)
+          .expect(({ body }) => {
+            expect(body).toHaveLength(1);
+            expect(body[0]).toMatchObject(notification[0]);
+          }));
     });
   });
 });
