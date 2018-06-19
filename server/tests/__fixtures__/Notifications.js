87fe054317af0a53facde0a45ba0614f7107eae9
devmonster423 <devmonster423@gmail.com>
2018-06-19 22:24:31 +0530

    Added more data.

--- server/tests/__fixtures__/Notifications.js
+++ server/tests/__fixtures__/Notifications.js
@@ -1,9 +1,19 @@
-const notification = {
-  title: 'Voluptatum ',
-  description: 'Neque ',
-  file: 'http:',
-  tags: ['tag1'],
-  link: 'https',
-};
+const notification = [
+  {
+    title: 'Voluptatum',
+    description: 'Ut id consequuntur dolor quo aut corrupti.',
+    file: 'https://melyna.com',
+    tags: ['student', 'iyear'],
+    link: 'http://rasheed.name',
+  },
+  {
+    title: 'Cletus',
+    description:
+      'Quisquam explicabo sit sunt debitis molestias inventore deleniti exercitationem vitae. Ut quam rerum ab quod illum ipsum soluta.',
+    file: 'https://candelario.org',
+    tags: ['pdf', 'teacher'],
+    link: 'http://noe.net',
+  },
+];
 
 module.exports = { notification };
