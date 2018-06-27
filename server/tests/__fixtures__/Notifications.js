cc97f7ca11451ba1f9096b7281b672aacfc5333c
devmonster423 <devmonster423@gmail.com>
2018-06-27 19:53:10 +0530

    Added more fixtured data.

--- server/tests/__fixtures__/Notifications.js
+++ server/tests/__fixtures__/Notifications.js
@@ -1,9 +1,11 @@
+const { lorem, image, internet } = require('faker');
+
 const notification = [
   {
     title: 'Voluptatum',
     description: 'Ut id consequuntur dolor quo aut corrupti.',
     file: 'https://melyna.com',
-    tags: ['student', 'iyear'],
+    tags: ['student', 'iyear', 'iiyear', 'teacher', 'pdf'],
     link: 'http://rasheed.name',
   },
   {
@@ -13,6 +15,27 @@ const notification = [
     file: 'https://candelario.org',
     tags: ['pdf', 'teacher'],
     link: 'http://noe.net',
+  },
+  {
+    title: lorem.sentence(),
+    description: lorem.paragraph(),
+    file: image.image(),
+    tags: ['pdf', 'teacher'],
+    link: internet.url(),
+  },
+  {
+    title: lorem.sentence(),
+    description: lorem.paragraph(),
+    file: image.image(),
+    tags: ['pdf', 'teacher'],
+    link: internet.url(),
+  },
+  {
+    title: lorem.sentence(),
+    description: lorem.paragraph(),
+    file: image.image(),
+    tags: ['pdf', 'teacher'],
+    link: internet.url(),
   },
 ];
 
