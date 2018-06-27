9e4b4271597b7679b0abc4cc4c6abc247f18cd37
devmonster423 <devmonster423@gmail.com>
2018-06-27 19:52:41 +0530

    Removed comments and typo.

--- server/utils/urls.js
+++ server/utils/urls.js
@@ -1,12 +1,10 @@
-// ------- visitiors routes urls ---------
-
 const GET_NOTIFICATIONS = '/s/visitor/getnotifications';
 const GET_ALL_NOTIFICATIONS = '/s/visitor/getallnotifications';
-const GET_ALL_QUESTION_PAPAER = '/s/visitor/getallQuestionPaper';
+const GET_ALL_QUESTION_PAPER = '/s/visitor/getallQuestionPaper';
 const GET_EVENTS = '/s/visitor/getevents';
 const GET_ALL_EVENTS = '/s/visitor/getallevents';
 const GET_SYLLABUS = '/s/visitor/getsyll';
-const GET_TIMETABEL = '/s/visitor/gettt';
+const GET_TIMETABLE = '/s/visitor/gettt';
 const GET_TEACHER = '/s/visitor/getteacher';
 const GET_STUDENT = '/s/visitor/getstudent';
 const SEARCH_STUDENT_BY_NAME = '/s/visitor/searchstudentbyname';
@@ -16,11 +14,11 @@ const VERIFY_EMAIL = '/s/visitor/verifyemail';
 module.exports = {
   GET_NOTIFICATIONS,
   GET_ALL_NOTIFICATIONS,
-  GET_ALL_QUESTION_PAPAER,
+  GET_ALL_QUESTION_PAPER,
   GET_EVENTS,
   GET_ALL_EVENTS,
   GET_SYLLABUS,
-  GET_TIMETABEL,
+  GET_TIMETABLE,
   GET_TEACHER,
   GET_STUDENT,
   SEARCH_STUDENT_BY_NAME,
